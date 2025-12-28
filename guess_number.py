00f978bae34947aa9201a8952d4a8383cc39950f
Kirill Gruzdev <gruzdev.krll@yandex.ru>
2025-12-28 21:35:27 +0300

    add initial commit

---
+++ guess_number.py
@@ -0,0 +1,14 @@
+from random import randint
+
+the_number = randint(1, 100)
+user_number = 0
+while True:
+    user_number = int(input())
+    if user_number == the_number:
+        print('Отличная интуиция! Вы угадали число :)')
+        break
+    elif user_number > the_number:
+        print('Ваше число больше того, что загадано')
+    else:
+        print('Ваше число меньше того, что загадано')
+    
